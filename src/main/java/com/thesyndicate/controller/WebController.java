60c21e96dc7cfaf897c426e4dc180eec10831637
Rafael Marcos <rafix995@gmail.com>
2023-06-28 22:13:22 +0100

    Sign up action flow in case of success.

--- src/main/java/com/thesyndicate/controller/WebController.java
+++ src/main/java/com/thesyndicate/controller/WebController.java
@@ -17,12 +17,23 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class WebController {
 	private final String LOGIN_ERROR = "LOGIN_ERROR";
+	private final String LOGIN_ERROR_PARAM = "login_error";
 	private final String CAPTCHA_ERROR = "CAPTCHA_ERROR";
+	private final String CAPTCHA_ERROR_PARAM = "captcha_error";
 	private final String LOGIN_SUCCESS = "LOGIN_SUCCESS";
+	private final String LOGIN_SUCCESS_PARAM = "login_success";
+	private final String SIGNUP_ERROR = "SIGNUP_ERROR";
+	private final String CAPTCHA_WRAPPER = "captchaWrapper";
+	private final String SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+	private final String SIGNUP_SUCCESS_PARAM = "signup_success";
 
 	@Autowired
 	private UserController userController;
 	private CaptchaWrapper captchaWrapper;
+
+	public WebController(){
+		this.captchaWrapper = new CaptchaWrapper();
+	}
 
 	@GetMapping(value = {"/", "index", "home"})
 	public String index() {
@@ -35,21 +46,20 @@ public class WebController {
 	 * @return
 	 */
 	@GetMapping(value = "/login")
-	public String login(
-			Model model,
-			@RequestParam(value = "loginError", required = false) boolean loginError,
-			@RequestParam(value = "captchaError", required = false) boolean captchaError,
-			@RequestParam(value = "loginSuccess", required = false) boolean loginSuccess
-			) {
+	public String login(Model model,
+						@RequestParam(value = LOGIN_ERROR_PARAM, required = false) boolean loginError,
+						@RequestParam(value = CAPTCHA_ERROR_PARAM, required = false) boolean captchaError,
+						@RequestParam(value = LOGIN_SUCCESS_PARAM, required = false) boolean loginSuccess,
+						@RequestParam(value = SIGNUP_SUCCESS_PARAM, required = false) boolean signUpSuccess) {
 
 		model.addAttribute(LOGIN_ERROR, loginError);
 		model.addAttribute(CAPTCHA_ERROR, captchaError);
+		model.addAttribute(SIGNUP_SUCCESS, signUpSuccess);
 
-		this.captchaWrapper = new CaptchaWrapper();
-		model.addAttribute("captchaWrapper", this.captchaWrapper);
+		model.addAttribute(CAPTCHA_WRAPPER, this.captchaWrapper);
 
 		//log
-		System.out.println("loginSuccess="+loginSuccess);
+		System.out.println(LOGIN_SUCCESS_PARAM + " : " + loginSuccess);
 
 		if(loginSuccess) return "redirect:/home";
 		return "login";
@@ -64,11 +74,10 @@ public class WebController {
 	 * @return RedirectView object that redirects to the login page (GET request)
 	 */
 	@PostMapping(value = "/login")
-	public RedirectView login(
-			String username,
-			String password,
-			@ModelAttribute("captchaWrapper") CaptchaWrapper captchaWrapper,
-			Model model) {
+	public RedirectView login(String username,
+							  String password,
+							  @ModelAttribute("captchaWrapper") CaptchaWrapper captchaWrapper,
+							  Model model) {
 
 		StringBuilder suffix = new StringBuilder();
 
@@ -78,7 +87,7 @@ public class WebController {
 			//System.err.println(UserKt.encryptPassword("root"));
 			if (Objects.isNull(user)) {
 				System.out.println("Login failed");
-				suffix.append("?loginError=true");//model.addAttribute(LOGIN_ERROR, true);
+				suffix.append("?" + LOGIN_ERROR_PARAM + "=true");//model.addAttribute(LOGIN_ERROR, true);
 			}
 			else {
 				System.out.println("Welcome");
@@ -86,8 +95,28 @@ public class WebController {
 				model.addAttribute(LOGIN_SUCCESS, true);
 			}
 		}else{
-			suffix.append(suffix.toString().length() > 0 ? "&captchaError=true": "?captchaError=true");//model.addAttribute(CAPTCHA_ERROR, true);
+			suffix.append(suffix.toString().length() > 0 ? "&" + CAPTCHA_ERROR_PARAM + "=true": "?" + CAPTCHA_ERROR_PARAM + "=true");//model.addAttribute(CAPTCHA_ERROR, true);
 		}
 		return new RedirectView("/login" + suffix.toString());
+	}
+
+	@GetMapping(value = "/register_user")
+	public String registerUser(@RequestParam(required = false) boolean signUpError,
+							   Model model){
+		if(signUpError)
+			model.addAttribute(SIGNUP_ERROR, true);
+		model.addAttribute(CAPTCHA_WRAPPER, this.captchaWrapper);
+
+		return "register_user";
+	}
+
+	@PostMapping(value = "/register_user")
+	public String registerUser(Model model,
+							   @RequestParam String username,
+							   @RequestParam String pass,
+							   @RequestParam String cpass,
+							   @ModelAttribute String captchaWrapper){
+
+		return "redirect:/login?" + SIGNUP_SUCCESS_PARAM + "=true";
 	}
 }
